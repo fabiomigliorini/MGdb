d68c51b21b05f18d27a14129fe0e1f55ce222c13
Fabio Migliorini <fabio.migliorini@gmail.com>
2022-10-29 16:42:19 -0400

    [ADD] ProdutoImagemProdutoVariacao

--- SQLs/Vendas de Papel.sql
+++ SQLs/Vendas de Papel.sql
@@ -5,6 +5,8 @@
      --, tblsubgrupoproduto.subgrupoproduto
        tblmarca.codmarca
      , tblmarca.marca
+     , tblnaturezaoperacao.codnaturezaoperacao
+     , tblnaturezaoperacao.naturezaoperacao
      , tblfilial.codfilial
      , tblfilial.filial
      , tblproduto.codproduto
@@ -19,13 +21,19 @@
   left join tblfilial              on (tblfilial.codfilial                     = tblnegocio.codfilial)
   left join tblprodutobarra        on (tblprodutobarra.codprodutobarra         = tblnegocioprodutobarra.codprodutobarra)
   left join tblproduto             on (tblproduto.codproduto                   = tblprodutobarra.codproduto)
-  --left join tblsubgrupoproduto     on (tblsubgrupoproduto.codsubgrupoproduto   = tblproduto.codsubgrupoproduto)
+  left join tblsubgrupoproduto     on (tblsubgrupoproduto.codsubgrupoproduto   = tblproduto.codsubgrupoproduto)
   --left join tblgrupoproduto        on (tblgrupoproduto.codgrupoproduto         = tblsubgrupoproduto.codgrupoproduto)
   left join tblmarca               on (tblmarca.codmarca                       = coalesce(tblprodutobarra.codmarca, tblproduto.codmarca))
   left join tblprodutoembalagem    on (tblprodutoembalagem.codprodutoembalagem = tblprodutobarra.codprodutoembalagem)
- where tblnegocio.codoperacao       = 2 -- saida
-   and tblnegocio.codnegociostatus  = 2 -- fechado
---   and tblnegocio.lancamento       >= '2016-01-01 00:00:00.0'
+  left join tblnaturezaoperacao    on (tblnaturezaoperacao.codnaturezaoperacao = tblnegocio.codnaturezaoperacao)
+ where tblnegocio.codnegociostatus  = 2 -- fechado
+   and tblnegocio.lancamento       >= '2020-11-01 00:00:00.0'
 --   and tblnegocio.lancamento       <= '2015-12-31 23:59:59.9'
-   and tblnegocio.codpessoa         not in (select tblfilial.codpessoa from tblfilial)
-   and tblproduto.codsubgrupoproduto = 00014010
+   --and tblnegocio.codpessoa         not in (select tblfilial.codpessoa from tblfilial)
+   --AND tblproduto.codproduto IN (30532, 30825, 46572, 47187, 49582, 47570, 53670, 54396)
+   and tblsubgrupoproduto.codgrupoproduto = 1070
+   and (tblnaturezaoperacao.venda or tblnaturezaoperacao.vendadevolucao or tblnaturezaoperacao.compra)
+   --and tblproduto.codsubgrupoproduto = 00014010
+   
+   
+select * from tblnaturezaoperacao t where venda = true
