d55a18255e662574523ee61beba620880c4d6b36
Fabio Migliorini <fabio@mgpapelaria.com.br>
2024-06-15 12:38:07 -0400

    [UPD] SQLs

--- SQLs/Negocios - Recebidos PagarMe Stone PIX nao fechados.sql
+++ SQLs/Negocios - Recebidos PagarMe Stone PIX nao fechados.sql
@@ -1,4 +1,4 @@
-select f.filial, u.usuario, count(*), sum(n.valortotal), max(n.codnegocio), max(n.lancamento)
+select f.filial, u.usuario, 'PagarMe', count(*), sum(n.valortotal), max(n.codnegocio), max(n.lancamento)
 from tblpagarmepedido pp 
 inner join tblnegocio n on (n.codnegocio = pp.codnegocio)
 inner join tblfilial f on (f.codfilial = n.codfilial)
@@ -6,10 +6,8 @@ inner join tblusuario u on (u.codusuario = n.codusuario)
 where pp.status = 2
 and n.codnegociostatus = 1
 group by f.filial, u.usuario
-order by 1, 2
-
-
-select f.filial, u.usuario, count(*), sum(n.valortotal), max(n.codnegocio), max(n.lancamento)
+union all
+select f.filial, u.usuario, 'PIX', count(*), sum(n.valortotal), max(n.codnegocio), max(n.lancamento)
 from tblpix p
 inner join tblpixcob pc on (pc.codpixcob = p.codpixcob)
 inner join tblnegocio n on (n.codnegocio = pc.codnegocio)
@@ -17,4 +15,4 @@ inner join tblfilial f on (f.codfilial = n.codfilial)
 inner join tblusuario u on (u.codusuario = n.codusuario)
 where n.codnegociostatus = 1
 group by f.filial, u.usuario
-order by 1, 2
+order by 1, 2, 3
