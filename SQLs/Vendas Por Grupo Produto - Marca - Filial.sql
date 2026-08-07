d10288fc6946935ac5958f23c688f66677086624
Fabio Migliorini <fabio.migliorini@gmail.com>
2026-08-07 12:05:10 -0400

    [UPD] SQLs

--- SQLs/Vendas Por Grupo Produto - Marca - Filial.sql
+++ SQLs/Vendas Por Grupo Produto - Marca - Filial.sql
@@ -26,4 +26,72 @@
    and tblnegocio.lancamento       >= '2013-06-01 00:00:00.0'
    and tblnegocio.lancamento       <= '2013-07-31 23:59:59.9'
    and tblnegocio.codpessoa         not in (select tblfilial.codpessoa from tblfilial)
- 
+   AND tblnegocioprodutobarra.inativo IS NULL
+   
+   
+   
+   select 
+       tblproduto.codproduto
+     , tblproduto.produto
+     --, tblnegocio.codnegocio
+     --, tblnegocio.lancamento
+     , sum(tblnegocioprodutobarra.quantidade * coalesce(tblprodutoembalagem.quantidade, 1)) as quantidade
+     , sum(tblnegocioprodutobarra.valortotal) as valor
+  from tblnegocio 
+  left join tblnegocioprodutobarra on (tblnegocioprodutobarra.codnegocio       = tblnegocio.codnegocio)
+  left join tblprodutobarra        on (tblprodutobarra.codprodutobarra         = tblnegocioprodutobarra.codprodutobarra)
+  left join tblproduto             on (tblproduto.codproduto                   = tblprodutobarra.codproduto)
+  left join tblprodutoembalagem    on (tblprodutoembalagem.codprodutoembalagem = tblprodutobarra.codprodutoembalagem)
+  left join tblnaturezaoperacao    on (tblnaturezaoperacao.codnaturezaoperacao = tblnegocio.codnaturezaoperacao)
+ where tblnegocio.codnegociostatus  = 2 -- fechado
+   and tblnegocio.lancamento       >= '2025-07-31 00:00:00.0'
+   and tblnegocio.lancamento       <= '2026-07-30 23:59:59.9'
+   and tblnegocio.codpessoa         not in (select tblfilial.codpessoa from tblfilial)
+   AND tblnegocioprodutobarra.inativo IS null
+   and tblnaturezaoperacao.venda = true
+  group by 
+       tblproduto.codproduto
+     , tblproduto.produto
+  order by 4 desc
+  
+  
+  
+  
+  
+   select 
+       tblproduto.codproduto
+     , tblproduto.produto
+     --, tblnegocio.codnegocio
+     --, tblnegocio.lancamento
+     , sum(tblnegocioprodutobarra.quantidade * coalesce(tblprodutoembalagem.quantidade, 1)) as quantidade
+     , sum(tblnegocioprodutobarra.valortotal) as valor
+  from tblnegocio 
+  left join tblnegocioprodutobarra on (tblnegocioprodutobarra.codnegocio       = tblnegocio.codnegocio)
+  left join tblprodutobarra        on (tblprodutobarra.codprodutobarra         = tblnegocioprodutobarra.codprodutobarra)
+  left join tblproduto             on (tblproduto.codproduto                   = tblprodutobarra.codproduto)
+  left join tblprodutoembalagem    on (tblprodutoembalagem.codprodutoembalagem = tblprodutobarra.codprodutoembalagem)
+  left join tblnaturezaoperacao    on (tblnaturezaoperacao.codnaturezaoperacao = tblnegocio.codnaturezaoperacao)
+ where tblnegocio.codnegociostatus  = 2 -- fechado
+   and tblnegocio.lancamento       >= '2025-07-31 00:00:00.0'
+   and tblnegocio.lancamento       <= '2026-07-30 23:59:59.9'
+   and tblnegocio.codpessoa         not in (select tblfilial.codpessoa from tblfilial)
+   AND tblnegocioprodutobarra.inativo IS null
+   and tblnaturezaoperacao.venda = true
+  group by 
+       tblproduto.codproduto
+     , tblproduto.produto
+  order by 4 desc
+
+  
+  
+select 
+	date_trunc('month', nf.emissao) as mes, 
+	sum(nf.valortotal) as valor
+from tblnotafiscal nf
+inner join tblnaturezaoperacao nat on (nat.codnaturezaoperacao = nf.codnaturezaoperacao)
+where nf.codfilial in (101, 102, 103, 104, 105)
+and nf.emitida 
+and nf.status = 'AUT'
+and nat.venda = true
+group by date_trunc('month', nf.emissao)
+order by mes desc
